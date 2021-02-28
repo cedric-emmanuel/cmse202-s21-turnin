2514b15fb172a5fc4c2e7049c0342da03e4f97a2
unknown <emmanu11@msu.edu>
2021-03-01 04:57:36 +0530

    updated class

--- hw02/Student.py
+++ hw02/Student.py
@@ -1,4 +1,3 @@
-# The Student class (you'll edit and expand on this)
 class Student():
     '''
     This class is designed to include information about individual students.
@@ -8,12 +7,32 @@ class Student():
     gpa : this is the student's curret gpa
     '''
     
-    def __init__(self, name='', gpa=0.0):
+    def __init__(self, name='', gpa=0.0,year=0):
         self.name = name
         self.gpa = gpa
-        
+        self.year=year
+        self.courses=[]
     def get_name(self):
         '''
         This function prints the name of the student
         '''
         print("My name is", self.name)
+        
+    def enrol(self,course_list):
+        self.courses.append(course_list)
+
+    def display_courses(self):
+        print("I am enroled in ",self.courses)
+    def years_until_graduation(self):
+        return 4-self.year
+
+    
+class Spartan(Student):
+    def __init__(self,name):
+        super().__init__(self,name)
+        self.motto=[]
+    def set_motto(self,m):
+        self.motto=m
+    def school_spirit(self):
+        print("My name is:",self.name)
+        print("I am a Spartan. My motto is",self.motto)
